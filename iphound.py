c48bf18354230b599bf6436bb3421cd0b11d7c8d
Tyler <tyler@carlisletech.com>
2022-08-15 22:35:08 -0500

    df

--- iphound.py
+++ iphound.py
@@ -20,7 +20,7 @@ def ipinfo():
 
 def dnsTrail():
     headers = {'Accept': 'application/json', 'APIKEY': 'pYIVSi6qspPzwXL6FJM9QQNCfxPCXboE'}
-    url = 'https://api.securitytrails.com/v1/ip/' + ip
+    url = 'https://api.securitytrails.com/v1/ips/' + ip
     r= requests.get(url, headers=headers)
     print(r.json())
 
